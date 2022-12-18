c4aac4f97f8314592fd906c0e5f03310990b1ad6
rocha <rjoao0112@gmail.com>
2022-12-18 22:07:13 +0000

    Set Pokemon Deck Implementation

--- src/GAME/PLAYER/Player.java
+++ src/GAME/PLAYER/Player.java
@@ -6,6 +6,7 @@ import POKEMON.*;
 import POKEMON.ALL_POKEMONS.Ekans;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -35,6 +36,243 @@ public class Player {
 
     //Method's
 
+    //Chose Pokemons to deck
+    public void setPokemonsDeck(){
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while(true){
+            System.out.println();
+            System.out.println("1 - Show Deck");
+            System.out.println("2 - Show All Pokemons");
+            System.out.println("3 - Remove Pokemon on Deck");
+            System.out.println("4 - Set Pokemon on Deck");
+            System.out.println("5 - Clean Deck");
+            System.out.println();
+            System.out.println("X - EXIT");
+            System.out.println();
+
+            System.out.println("Chose a option: ");
+            input = sc.next();
+
+            if (input.equals("x") || input.equals("X")){
+                System.out.println();
+                break;
+            }
+
+            switch (input){
+                case "1" -> {
+                    showPokemonDeck();
+                }
+                case "2" -> {
+                    showAllPokemons();
+                }
+                case "3" -> {
+                    removePokemonOnDeck();
+                }
+                case "4" -> {
+                    setPokemonOnDeck();
+                }
+                case "5" -> {
+                    cleanDeck();
+                }
+                default -> {}
+            }
+
+        }
+    }
+
+    //Remove Pokemon on deck
+    private void removePokemonOnDeck(){
+        Scanner sc = new Scanner(System.in);
+        String input;
+        int numberInput;
+
+        while(true){
+
+            System.out.println("What Pokemon do you want to remove of deck? (Ex:/ 2)");
+            input = sc.next();
+
+            try {
+                numberInput = Integer.parseInt(input);
+            } catch (NumberFormatException nfe) {
+                System.out.println();
+                System.out.println("Not valid input!");
+                continue;
+            }
+
+            if (!(numberInput >= 1) || !(numberInput <= pokemonDeck.length)){
+                System.out.println("Not valid position!");
+                continue;
+            }
+
+            pokemonDeck[numberInput - 1] = null;
+
+            if (numberInput != pokemonDeck.length) {
+                for (int i = 0; i < pokemonDeck.length; i++) {
+                    if (i == pokemonDeck.length - 1) {
+                        break;
+                    }
+                    if (Objects.isNull(pokemonDeck[i]) && !Objects.isNull(pokemonDeck[i + 1])){
+                        pokemonDeck[i] = pokemonDeck[i+1];
+                        pokemonDeck[i+1] = null;
+                    }
+                }
+            }
+
+            while (true) {
+                System.out.println();
+                System.out.println("Still want to set pokemon's on deck?");
+                System.out.println();
+                System.out.println("1 - YES");
+                System.out.println("0 - NO");
+                System.out.println();
+                input = sc.next();
+                if (input.equals("1") || input.equals("0")) {
+                    break;
+                }
+            }
+
+            if (input.equals("0")){
+                break;
+            }
+        }
+    }
+
+    //Set Pokemon on Deck
+    private void setPokemonOnDeck(){
+        Scanner sc = new Scanner(System.in);
+        String input;
+        PokemonImp tempPokemon;
+
+        while(true){
+            tempPokemon = catchPokemon();
+            verifyPositionOnDeck(tempPokemon);
+
+            while (true) {
+                System.out.println();
+                System.out.println("Still want to set pokemon's on deck?");
+                System.out.println();
+                System.out.println("1 - YES");
+                System.out.println("0 - NO");
+                System.out.println();
+                input = sc.next();
+                if (input.equals("1") || input.equals("0")) {
+                    break;
+                }
+            }
+
+            if (input.equals("0")){
+                break;
+            }
+        }
+    }
+
+    //Verify position on Deck
+    private void verifyPositionOnDeck(PokemonImp pokemon){
+        Scanner sc = new Scanner(System.in);
+        String input;
+        int numberInput;
+        boolean existNull = false;
+        boolean existOnDeck = false;
+
+        while (true) {
+            System.out.println();
+            System.out.println("Chose a position on deck! (ex:/ 5 )");
+            System.out.println();
+            input = sc.next();
+
+            try {
+                numberInput = Integer.parseInt(input);
+            } catch (NumberFormatException nfe) {
+                System.out.println();
+                System.out.println("Not valid input!");
+                continue;
+            }
+
+            if (!(numberInput >= 1) || !(numberInput <= pokemonDeck.length)) {
+                System.out.println();
+                System.out.println("Invalid Position!");
+                continue;
+            }
+
+            //Verify if position is null and if it's the first position null
+            //If not this will find the first position null and set there the pokemon
+            if (Objects.isNull(pokemonDeck[numberInput - 1])){
+                for (int i = numberInput - 1; i > 0; i--){
+                    if ((i - 1) == 0){
+                        pokemonDeck[0] = pokemon;
+                        break;
+                    }
+                    if (Objects.isNull(pokemonDeck[i - 1])){
+                        continue;
+                    }
+                    pokemonDeck[i] = pokemon;
+                    break;
+                }
+                System.out.println("Pokemon is Set");
+                return;
+            }
+
+            for (PokemonImp pokemonImp : pokemonDeck) {
+                if (pokemonImp == pokemon) {
+                    existOnDeck = true;
+                    break;
+                }
+            }
+            if (existOnDeck){
+                System.out.println("Already on Deck");
+                continue;
+            }
+
+            for (int i = 0; i < pokemonDeck.length ; i++){
+                if (Objects.isNull(pokemonDeck[i])){
+                    pokemonDeck[i] = pokemon;
+                    System.out.println("Pokemon is Set");
+                    return;
+                }
+            }
+
+            pokemonDeck[numberInput - 1] = pokemon;
+            System.out.println("Pokemon is Set");
+            return;
+        }
+    }
+
+    //Catch pokemon
+    private PokemonImp catchPokemon(){
+        Scanner sc = new Scanner(System.in);
+        String input;
+        int numberInput;
+
+        while (true) {
+            System.out.println();
+            System.out.println("Chose a pokemon of all pokemons! (ex:/ 5 )");
+            System.out.println();
+            input = sc.next();
+
+            try {
+                numberInput = Integer.parseInt(input);
+            } catch (NumberFormatException nfe) {
+                System.out.println();
+                System.out.println("Not valid input!");
+                continue;
+            }
+
+            if (!(numberInput >= 1) || !(numberInput <= allPokemons.size())) {
+                System.out.println();
+                System.out.println("Invalid Position!");
+                continue;
+            }
+
+            return allPokemons.get(numberInput - 1);
+        }
+    }
+
+    //Clean deck
+    private void cleanDeck(){
+        Arrays.fill(pokemonDeck, null);
+    }
+
     //Search on inventory
     public void addItemOnInventory(Item item, int quantity){
         if (inventory.size() == 0){
@@ -50,6 +288,44 @@ public class Player {
         }
 
         inventory.add(new InventorySpace(item, quantity));
+    }
+
+    //Show Items Inventory
+    public void showInventory(){
+        System.out.println();
+        for (InventorySpace i : inventory){
+            System.out.println(i.getItem().getName());
+            System.out.println("Num of Items: "+ i.getQuantity());
+            System.out.println();
+        }
+    }
+
+    //Show all Pokemon's
+    public void showAllPokemons(){
+        System.out.println();
+        for (int i = 0; i < allPokemons.size(); i++){
+            System.out.println(i+". ");
+            System.out.println(allPokemons.get(i).getName());
+            System.out.println("Health: " + allPokemons.get(i).getHealth() + "/" + allPokemons.get(i).getMaxHealth());
+            System.out.println("Damage: " + allPokemons.get(i).getDamage());
+            System.out.println();
+        }
+    }
+
+    //Show Pokemon Deck
+    public void showPokemonDeck(){
+        System.out.println();
+        for (int i = 0; i < pokemonDeck.length; i++){
+            if (Objects.isNull(pokemonDeck[i])){
+                System.out.println();
+                return;
+            }
+            System.out.println(i+". ");
+            System.out.println(pokemonDeck[i].getName());
+            System.out.println("Health: " + pokemonDeck[i].getHealth() + "/" + pokemonDeck[i].getMaxHealth());
+            System.out.println("Damage: " + pokemonDeck[i].getDamage());
+            System.out.println();
+        }
     }
 
     //Chose Pokemon to attack
